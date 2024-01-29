66f9633586db7b7a4024ba28dfdf041e3b5ac812
yctai1994 <yctai.git@gmail.com>
2024-01-29 14:48:53 +0900

    add: `zig.blas.geru` (#2)

--- zig/src/blas.zig
+++ zig/src/blas.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const testing = std.testing;
 
+const Transpose = enum(u1) { N, T }; // (N, T): (Normal, Transpose)
+const WriteMode = enum(u1) { W, U }; // (W, U): (Overwrite, Update)
+
 pub fn copy(comptime T: type, des: []T, src: []T) void {
     if (@typeInfo(T) != .Float) @compileError("copy(T, ...): T should be a float type.");
     for (des, src) |*pd, vs| pd.* = vs;
@@ -11,6 +14,7 @@ test "blas.copy" {
     const allocator = testing.allocator;
     const src: []f32 = try allocator.alloc(f32, 10);
     const des: []f32 = try allocator.alloc(f32, 10);
+
     defer {
         allocator.free(src);
         allocator.free(des);
@@ -19,6 +23,7 @@ test "blas.copy" {
     for (src, 0..) |*p, i| p.* = @floatFromInt(i);
 
     copy(f32, des, src);
+
     for (des, src) |vd, vs| try testing.expectEqual(vd, vs);
 }
 
@@ -33,6 +38,7 @@ test "blas.xoty" {
     const x: []f32 = try allocator.alloc(f32, 10);
     const y: []f32 = try allocator.alloc(f32, 10);
     const z: []f32 = try allocator.alloc(f32, 10);
+
     defer {
         allocator.free(x);
         allocator.free(y);
@@ -45,10 +51,9 @@ test "blas.xoty" {
     }
 
     xoty(f32, x, y, z);
+
     for (z, 0..) |v, i| try testing.expectEqual(v, @as(f32, @floatFromInt(6 * i * i)));
 }
-
-const Transpose = enum(u1) { N, T };
 
 pub fn gemv(comptime T: type, comptime tA: Transpose, a: T, A: [][]T, x: []T, b: T, y: []T, z: []T) void {
     if (@typeInfo(T) != .Float) @compileError("gemv(T, ...): T should be a float type.");
@@ -110,7 +115,7 @@ pub fn gemv(comptime T: type, comptime tA: Transpose, a: T, A: [][]T, x: []T, b:
     return;
 }
 
-test "gemv.N" {
+test "blas.gemv.N" {
     const allocator = testing.allocator;
     const A: [][]f32 = blk: {
         const mat: [][]f32 = try allocator.alloc([]f32, 4);
@@ -138,9 +143,9 @@ test "gemv.N" {
         allocator.free(z);
     }
 
-    const a_list: [3]f32 = .{ 0.0, 1.0, 3.0 };
+    const a_list: [4]f32 = .{ 0.0, 1.0, -1.0, 3.0 };
     const b_list: [4]f32 = .{ 0.0, 1.0, -1.0, -5.0 };
-    const z_list: [12][4]f32 = .{
+    const z_list: [16][4]f32 = .{
         .{ 0.0, 0.0, 0.0, 0.0 },
         .{ -1.0, 3.0, -5.0, 1.0 },
         .{ 1.0, -3.0, 5.0, -1.0 },
@@ -149,6 +154,10 @@ test "gemv.N" {
         .{ 19.0, 19.0, 21.0, 15.0 },
         .{ 21.0, 13.0, 31.0, 13.0 },
         .{ 25.0, 1.0, 51.0, 9.0 },
+        .{ -20.0, -16.0, -26.0, -14.0 },
+        .{ -21.0, -13.0, -31.0, -13.0 },
+        .{ -19.0, -19.0, -21.0, -15.0 },
+        .{ -15.0, -31.0, -1.0, -19.0 },
         .{ 60.0, 48.0, 78.0, 42.0 },
         .{ 59.0, 51.0, 73.0, 43.0 },
         .{ 61.0, 45.0, 83.0, 41.0 },
@@ -159,14 +168,12 @@ test "gemv.N" {
         for (b_list, 0..) |b, ib| {
             gemv(f32, .N, a, A, x, b, y, z);
 
-            for (z_list[4 * ia + ib], z) |va, vz| {
-                try testing.expectEqual(va, vz);
-            }
+            try testing.expect(std.mem.eql(f32, &z_list[4 * ia + ib], z));
         }
     }
 }
 
-test "gemv.T" {
+test "blas.gemv.T" {
     const allocator = testing.allocator;
     const A: [][]f32 = blk: {
         const mat: [][]f32 = try allocator.alloc([]f32, 4);
@@ -194,9 +201,9 @@ test "gemv.T" {
         allocator.free(z);
     }
 
-    const a_list: [3]f32 = .{ 0.0, 1.0, 3.0 };
+    const a_list: [4]f32 = .{ 0.0, 1.0, -1.0, 3.0 };
     const b_list: [4]f32 = .{ 0.0, 1.0, -1.0, -5.0 };
-    const z_list: [12][3]f32 = .{
+    const z_list: [16][3]f32 = .{
         .{ 0.0, 0.0, 0.0 },
         .{ -1.0, 2.0, 1.0 },
         .{ 1.0, -2.0, -1.0 },
@@ -205,6 +212,10 @@ test "gemv.T" {
         .{ 35.0, -143.0, 4.0 },
         .{ 37.0, -147.0, 2.0 },
         .{ 41.0, -155.0, -2.0 },
+        .{ -36.0, 145.0, -3.0 },
+        .{ -37.0, 147.0, -2.0 },
+        .{ -35.0, 143.0, -4.0 },
+        .{ -31.0, 135.0, -8.0 },
         .{ 108.0, -435.0, 9.0 },
         .{ 107.0, -433.0, 10.0 },
         .{ 109.0, -437.0, 8.0 },
@@ -215,9 +226,151 @@ test "gemv.T" {
         for (b_list, 0..) |b, ib| {
             gemv(f32, .T, a, A, x, b, y, z);
 
-            for (z_list[4 * ia + ib], z) |va, vz| {
-                try testing.expectEqual(va, vz);
+            try testing.expect(std.mem.eql(f32, &z_list[4 * ia + ib], z));
+        }
+    }
+}
+
+pub fn geru(comptime T: type, mode: WriteMode, a: T, x: []T, y: []T, A: [][]T) void {
+    if (@typeInfo(T) != .Float) @compileError("geru(T, ...): T should be a float type.");
+
+    switch (mode) {
+        .W => {
+            if (a == 0.0) {
+                for (A) |row| {
+                    for (row) |*pr| pr.* = 0.0;
+                }
+            } else if (a == 1.0) {
+                for (A, x) |row, vx| {
+                    if (vx != 0.0) {
+                        for (row, y) |*pr, vy| pr.* = vx * vy;
+                    }
+                }
+            } else if (a == -1.0) {
+                var tmp: T = undefined;
+                for (A, x) |row, vx| {
+                    if (vx != 0.0) {
+                        tmp = -vx;
+                        for (row, y) |*pr, vy| pr.* = tmp * vy;
+                    }
+                }
+            } else {
+                var tmp: T = undefined;
+                for (A, x) |row, vx| {
+                    if (vx != 0.0) {
+                        tmp = a * vx;
+                        for (row, y) |*pr, vy| pr.* = tmp * vy;
+                    }
+                }
             }
+        },
+        .U => {
+            if (a == 0.0) {
+                return;
+            } else if (a == 1.0) {
+                for (A, x) |row, vx| {
+                    if (vx != 0.0) {
+                        for (row, y) |*pr, vy| pr.* += vx * vy;
+                    }
+                }
+            } else if (a == -1.0) {
+                for (A, x) |row, vx| {
+                    if (vx != 0.0) {
+                        for (row, y) |*pr, vy| pr.* -= vx * vy;
+                    }
+                }
+            } else {
+                var tmp: T = undefined;
+                for (A, x) |row, vx| {
+                    if (vx != 0.0) {
+                        tmp = a * vx;
+                        for (row, y) |*pr, vy| pr.* += tmp * vy;
+                    }
+                }
+            }
+        },
+    }
+
+    return;
+}
+
+test "blas.geru.W" {
+    const allocator = testing.allocator;
+    const A: [][]f32 = blk: {
+        const mat: [][]f32 = try allocator.alloc([]f32, 4);
+        for (mat) |*row| row.* = try allocator.alloc(f32, 3);
+        break :blk mat;
+    };
+
+    const x: []f32 = try allocator.alloc(f32, 4);
+    inline for (.{ -1.0, 3.0, -5.0, 1.0 }, x) |v, *p| p.* = v;
+
+    const y: []f32 = try allocator.alloc(f32, 3);
+    inline for (.{ 3.0, 1.0, 5.0 }, y) |v, *p| p.* = v;
+
+    defer {
+        for (A) |row| allocator.free(row);
+        allocator.free(A);
+        allocator.free(x);
+        allocator.free(y);
+    }
+
+    const a_list: [4]f32 = .{ 0.0, 1.0, -1.0, 3.0 };
+    const A_list: [4][4][3]f32 = .{
+        .{ .{ 0.0, 0.0, 0.0 }, .{ 0.0, 0.0, 0.0 }, .{ 0.0, 0.0, 0.0 }, .{ 0.0, 0.0, 0.0 } },
+        .{ .{ -3.0, -1.0, -5.0 }, .{ 9.0, 3.0, 15.0 }, .{ -15.0, -5.0, -25.0 }, .{ 3.0, 1.0, 5.0 } },
+        .{ .{ 3.0, 1.0, 5.0 }, .{ -9.0, -3.0, -15.0 }, .{ 15.0, 5.0, 25.0 }, .{ -3.0, -1.0, -5.0 } },
+        .{ .{ -9.0, -3.0, -15.0 }, .{ 27.0, 9.0, 45.0 }, .{ -45.0, -15.0, -75.0 }, .{ 9.0, 3.0, 15.0 } },
+    };
+
+    for (a_list, 0..) |a, ia| {
+        geru(f32, .W, a, x, y, A);
+
+        for (A_list[ia], A) |ans, row| {
+            try testing.expect(std.mem.eql(f32, &ans, row));
+        }
+    }
+}
+
+test "blas.geru.U" {
+    const allocator = testing.allocator;
+    const A: [][]f32 = blk: {
+        const mat: [][]f32 = try allocator.alloc([]f32, 4);
+        for (mat) |*row| row.* = try allocator.alloc(f32, 3);
+        break :blk mat;
+    };
+
+    const x: []f32 = try allocator.alloc(f32, 4);
+    inline for (.{ -1.0, 3.0, -5.0, 1.0 }, x) |v, *p| p.* = v;
+
+    const y: []f32 = try allocator.alloc(f32, 3);
+    inline for (.{ 3.0, 1.0, 5.0 }, y) |v, *p| p.* = v;
+
+    defer {
+        for (A) |row| allocator.free(row);
+        allocator.free(A);
+        allocator.free(x);
+        allocator.free(y);
+    }
+
+    const a_list: [4]f32 = .{ 0.0, 1.0, -1.0, 3.0 };
+    const A_list: [4][4][3]f32 = .{
+        .{ .{ 7.0, -11.0, 0.0 }, .{ 5.0, -17.0, 3.0 }, .{ 1.0, -13.0, 2.0 }, .{ 2.0, -19.0, 0.0 } },
+        .{ .{ 4.0, -12.0, -5.0 }, .{ 14.0, -14.0, 18.0 }, .{ -14.0, -18.0, -23.0 }, .{ 5.0, -18.0, 5.0 } },
+        .{ .{ 10.0, -10.0, 5.0 }, .{ -4.0, -20.0, -12.0 }, .{ 16.0, -8.0, 27.0 }, .{ -1.0, -20.0, -5.0 } },
+        .{ .{ -2.0, -14.0, -15.0 }, .{ 32.0, -8.0, 48.0 }, .{ -44.0, -28.0, -73.0 }, .{ 11.0, -16.0, 15.0 } },
+    };
+
+    for (a_list, 0..) |a, ia| {
+        inline for (.{ 7.0, -11.0, 0.0 }, A[0]) |v, *p| p.* = v;
+        inline for (.{ 5.0, -17.0, 3.0 }, A[1]) |v, *p| p.* = v;
+        inline for (.{ 1.0, -13.0, 2.0 }, A[2]) |v, *p| p.* = v;
+        inline for (.{ 2.0, -19.0, 0.0 }, A[3]) |v, *p| p.* = v;
+
+        geru(f32, .U, a, x, y, A);
+
+        for (A_list[ia], A) |ans, row| {
+            try testing.expect(std.mem.eql(f32, &ans, row));
         }
     }
 }
