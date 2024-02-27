1b93440be33e8f9e8102fc4778e8c4ae2a2e1f17
yctai1994 <yctai.git@gmail.com>
2024-02-28 00:35:52 +0900

    edit: `zig.layer` (#6)
    add: PReLU

--- zig/src/layer.zig
+++ zig/src/layer.zig
@@ -38,6 +38,13 @@ const Dense = struct {
         return .{ .w = w, .x = x, .b = b };
     }
 
+    fn deinit(self: *const Dense, arr: Array) void {
+        arr.free(self.w);
+        arr.free(self.x);
+        arr.free(self.b);
+        return;
+    }
+
     // z = W⋅x + b
     fn forward(self: *const Dense, z: []f64) void {
         // z ← b
@@ -82,13 +89,6 @@ const Dense = struct {
                 w_nm.* -= learn_rate * dcdz_n * x_m;
             }
         }
-        return;
-    }
-
-    fn deinit(self: *const Dense, arr: Array) void {
-        arr.free(self.w);
-        arr.free(self.x);
-        arr.free(self.b);
         return;
     }
 };
@@ -145,4 +145,108 @@ test "Dense.forward, Dense.backward, and Dense.learn" {
     try testing.expect(std.mem.eql(f64, &.{ 4.994, -2.010 }, dense.w[0]));
     try testing.expect(std.mem.eql(f64, &.{ 2.985, -5.025 }, dense.w[1]));
     try testing.expect(std.mem.eql(f64, &.{ 1.991, -3.015 }, dense.w[2]));
+}
+
+const PReLU = struct {
+    z: []f64, // n = 1:N
+    a: []f64, // n = 1:N
+
+    fn init(Arr: *const Array, args: *const LayerArgs, alpha: f64) !PReLU {
+        const z: []f64 = try Arr.vector(args.sz_o);
+        const a: []f64 = try Arr.vector(args.sz_o);
+
+        @memset(a, alpha);
+
+        return .{ .z = z, .a = a };
+    }
+
+    fn deinit(self: *const PReLU, arr: Array) void {
+        arr.free(self.z);
+        arr.free(self.a);
+        return;
+    }
+
+    // y = f(z; a)
+    fn forward(self: *const PReLU, y: []f64) void {
+        for (y, self.z, self.a) |*y_n, z_n, a_n| {
+            y_n.* = @max(0.0, z_n) + a_n * @min(0.0, z_n);
+        }
+
+        return;
+    }
+
+    // dc/dz ← dc/dy
+    fn backward(self: *const PReLU, dcdz: []f64, dcdy: []f64) void {
+        for (dcdz, dcdy, self.z, self.a) |*dcdz_n, dcdy_n, z_n, a_n| {
+            dcdz_n.* = if (z_n > 0.0) dcdy_n else dcdy_n * a_n;
+        }
+        return;
+    }
+
+    // a -= α ⋅ dc/da
+    fn learn(self: *const PReLU, dcdy: []f64, learn_rate: f64) void {
+        for (self.a, self.z, dcdy) |*a_n, z_n, dcdy_n| {
+            if (z_n < 0.0) a_n.* -= learn_rate * dcdy_n * z_n;
+        }
+        return;
+    }
+};
+
+test "PReLU.forward, PReLU.backward, and PReLU.learn" {
+    const ArrF64 = Array{ .allocator = testing.allocator };
+
+    const z: []f64 = try ArrF64.vector(3);
+    inline for (.{
+        -0x1.7f2e48e8a71dfp-3,
+        -0x1.4f0d844d013aap-3,
+        -0x1.16872b020c49cp-5,
+    }, z) |v, *p| p.* = v;
+
+    const a: []f64 = try ArrF64.vector(3);
+    inline for (.{
+        0x1.851eb851eb852p-3,
+        0x1.28f5c28f5c28fp-2,
+        0x1.5c28f5c28f5c3p-3,
+    }, a) |v, *p| p.* = v;
+
+    const prelu: PReLU = .{ .z = z, .a = a };
+    defer prelu.deinit(ArrF64);
+
+    // test "PReLU.forward"
+    const y: []f64 = try ArrF64.vector(3);
+    defer ArrF64.free(y);
+
+    prelu.forward(y);
+    try testing.expect(std.mem.eql(f64, y, &.{
+        -0x1.2337a80cf9e39p-5,
+        -0x1.84a9478c868bap-5,
+        -0x1.7acc4ef88b978p-8,
+    }));
+
+    // test "PReLU.backward"
+    const dcdy: []f64 = try ArrF64.vector(3);
+    inline for (.{
+        0x1.c28f5c28f5c29p-4,
+        0x1.5c28f5c28f5c3p-3,
+        0x1.0a3d70a3d70a4p-3,
+    }, dcdy) |v, *p| p.* = v;
+    defer ArrF64.free(dcdy);
+
+    const dcdz: []f64 = try ArrF64.vector(3);
+    defer ArrF64.free(dcdz);
+
+    prelu.backward(dcdz, dcdy);
+    try testing.expect(std.mem.eql(f64, dcdz, &.{
+        0x1.566cf41f212d8p-6,
+        0x1.93dd97f62b6afp-5,
+        0x1.6a161e4f765fep-6,
+    }));
+
+    // test "PReLU.learn"
+    prelu.learn(dcdy, 0.05);
+    try testing.expect(std.mem.eql(f64, prelu.a, &.{
+        0x1.873a3d12b005fp-3,
+        0x1.2a624c2572805p-2,
+        0x1.5c9cd3e0bd44ap-3,
+    }));
 }
