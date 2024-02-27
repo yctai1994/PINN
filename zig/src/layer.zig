347f3a0f4661ef204eaf8674c91a993b9377f1d4
yctai1994 <yctai.git@gmail.com>
2024-02-28 02:17:19 +0900

    edit: `zig.layer` (#3)
    1. add `Layer`
    2. unify unit-testing

--- zig/src/layer.zig
+++ zig/src/layer.zig
@@ -1,7 +1,4 @@
 const std = @import("std");
-const mem = std.mem;
-const testing = std.testing;
-
 const Array = @import("./array.zig").Array;
 const NormPrng = @import("./rand.zig").NormPrng;
 
@@ -14,18 +11,55 @@ fn invsqrt(x: f64) f64 {
 const LayerArgs = struct {
     sz_i: usize,
     sz_o: usize,
-    prng: NormPrng,
+    prng: *const NormPrng,
+};
+
+pub const Layer = struct {
+    dense: Dense,
+    prelu: PReLU,
+
+    pub fn init(allocator: std.mem.Allocator, args: *const LayerArgs) !Layer {
+        const ArrF64 = Array{ .allocator = allocator };
+        const alpha: f64 = invsqrt(0.5 * @as(f64, @floatFromInt(args.sz_i)));
+        return .{
+            .dense = try Dense.init(&ArrF64, args, alpha),
+            .prelu = try PReLU.init(&ArrF64, args, alpha),
+        };
+    }
+
+    pub fn deinit(self: *const Layer, allocator: std.mem.Allocator) void {
+        const ArrF64 = Array{ .allocator = allocator };
+        self.dense.deinit(&ArrF64);
+        self.prelu.deinit(&ArrF64);
+        return;
+    }
+
+    pub fn forward(self: *const Layer, y: []f64) void {
+        self.dense.forward(self.prelu.z);
+        self.prelu.forward(y);
+        return;
+    }
+
+    pub fn backward(self: *const Layer, dcdx: []f64, learn_rate: f64) void {
+        self.prelu.backward(self.dense.d);
+        self.prelu.learn(learn_rate);
+        self.dense.backward(dcdx);
+        self.dense.learn(learn_rate);
+        return;
+    }
 };
 
 const Dense = struct {
     w: [][]f64, // (nrow, ncol) = (n = 1:N, m = 1:M)
     x: []f64, // m = 1:M
     b: []f64, // n = 1:N
+    d: []f64, // dc/dz, n = 1:N
 
     fn init(Arr: *const Array, args: *const LayerArgs, alpha: f64) !Dense {
         const w: [][]f64 = try Arr.matrix(args.sz_o, args.sz_i);
         const x: []f64 = try Arr.vector(args.sz_i);
         const b: []f64 = try Arr.vector(args.sz_o);
+        const d: []f64 = try Arr.vector(args.sz_o);
 
         for (w) |row| {
             for (row) |*w_nm| {
@@ -35,13 +69,14 @@ const Dense = struct {
 
         @memset(b, 0.0);
 
-        return .{ .w = w, .x = x, .b = b };
+        return .{ .w = w, .x = x, .b = b, .d = d };
     }
 
-    fn deinit(self: *const Dense, arr: Array) void {
+    fn deinit(self: *const Dense, arr: *const Array) void {
         arr.free(self.w);
         arr.free(self.x);
         arr.free(self.b);
+        arr.free(self.d);
         return;
     }
 
@@ -64,10 +99,10 @@ const Dense = struct {
     }
 
     // dc/dx = Wᵀ ⋅ dc/dz
-    fn backward(self: *const Dense, dcdx: []f64, dcdz: []f64) void {
+    fn backward(self: *const Dense, dcdx: []f64) void {
         @memset(dcdx, 0.0);
 
-        for (self.w, dcdz) |row, dcdz_n| {
+        for (self.w, self.d) |row, dcdz_n| {
             for (row, dcdx) |w_mn, *dcdx_n| {
                 dcdx_n.* += w_mn * dcdz_n;
             }
@@ -78,13 +113,13 @@ const Dense = struct {
 
     // b -= α ⋅ dc/db => b -= α ⋅ dc/dz
     // W -= α ⋅ dc/dW => W -= α ⋅ dc/dz ⋅ xᵀ
-    fn learn(self: *const Dense, dcdz: []f64, learn_rate: f64) void {
+    fn learn(self: *const Dense, learn_rate: f64) void {
         // b -= α ⋅ dc/dz
-        for (self.b, dcdz) |*b_n, dcdz_n| {
+        for (self.b, self.d) |*b_n, dcdz_n| {
             b_n.* -= learn_rate * dcdz_n;
         }
         // W -= α ⋅ dc/dz ⋅ xᵀ
-        for (self.w, dcdz) |row, dcdz_n| {
+        for (self.w, self.d) |row, dcdz_n| {
             for (row, self.x) |*w_nm, x_m| {
                 w_nm.* -= learn_rate * dcdz_n * x_m;
             }
@@ -93,76 +128,25 @@ const Dense = struct {
     }
 };
 
-test "Dense.init" {
-    const ArrF64 = Array{ .allocator = testing.allocator };
-    const prng: NormPrng = try NormPrng.init(testing.allocator, null);
-    defer prng.deinit(testing.allocator);
-
-    const args: LayerArgs = .{ .sz_i = 512, .sz_o = 512, .prng = prng };
-    const alpha: f64 = invsqrt(0.5 * @as(f64, @floatFromInt(args.sz_i)));
-    const dense: Dense = try Dense.init(&ArrF64, &args, alpha);
-    defer dense.deinit(ArrF64);
-}
-
-test "Dense.forward, Dense.backward, and Dense.learn" {
-    const ArrF64 = Array{ .allocator = testing.allocator };
-
-    const W: [][]f64 = try ArrF64.matrix(3, 2);
-    inline for (.{ 5.0, -2.0 }, W[0]) |v, *p| p.* = v;
-    inline for (.{ 3.0, -5.0 }, W[1]) |v, *p| p.* = v;
-    inline for (.{ 2.0, -3.0 }, W[2]) |v, *p| p.* = v;
-
-    const b: []f64 = try ArrF64.vector(3);
-    inline for (.{ -2.0, -5.0, -1.0 }, b) |v, *p| p.* = v;
-
-    const x: []f64 = try ArrF64.vector(2);
-    inline for (.{ 0.3, 0.5 }, x) |v, *p| p.* = v;
-
-    const dense: Dense = .{ .w = W, .x = x, .b = b };
-    defer dense.deinit(ArrF64);
-
-    // test "Dense.forward"
-    const z: []f64 = try ArrF64.vector(3);
-    defer ArrF64.free(z);
-
-    dense.forward(z);
-    try testing.expect(std.mem.eql(f64, &.{ -1.5, -6.6, -1.9 }, z));
-
-    // test "Dense.backward"
-    const dcdz: []f64 = try ArrF64.vector(3);
-    inline for (.{ 0.2, 0.5, 0.3 }, dcdz) |v, *p| p.* = v;
-    defer ArrF64.free(dcdz);
-
-    const dcdx: []f64 = try ArrF64.vector(2);
-    defer ArrF64.free(dcdx);
-
-    dense.backward(dcdx, dcdz);
-    try testing.expect(std.mem.eql(f64, &.{ 3.1, -3.8 }, dcdx));
-
-    // test "Dense.learn"
-    dense.learn(dcdz, 0.1);
-    try testing.expect(std.mem.eql(f64, &.{ -2.02, -5.05, -1.03 }, dense.b));
-    try testing.expect(std.mem.eql(f64, &.{ 4.994, -2.010 }, dense.w[0]));
-    try testing.expect(std.mem.eql(f64, &.{ 2.985, -5.025 }, dense.w[1]));
-    try testing.expect(std.mem.eql(f64, &.{ 1.991, -3.015 }, dense.w[2]));
-}
-
 const PReLU = struct {
     z: []f64, // n = 1:N
     a: []f64, // n = 1:N
+    d: []f64, // dc/dy, n = 1:N
 
     fn init(Arr: *const Array, args: *const LayerArgs, alpha: f64) !PReLU {
         const z: []f64 = try Arr.vector(args.sz_o);
         const a: []f64 = try Arr.vector(args.sz_o);
+        const d: []f64 = try Arr.vector(args.sz_o);
 
         @memset(a, alpha);
 
-        return .{ .z = z, .a = a };
+        return .{ .z = z, .a = a, .d = d };
     }
 
-    fn deinit(self: *const PReLU, arr: Array) void {
+    fn deinit(self: *const PReLU, arr: *const Array) void {
         arr.free(self.z);
         arr.free(self.a);
+        arr.free(self.d);
         return;
     }
 
@@ -176,77 +160,115 @@ const PReLU = struct {
     }
 
     // dc/dz ← dc/dy
-    fn backward(self: *const PReLU, dcdz: []f64, dcdy: []f64) void {
-        for (dcdz, dcdy, self.z, self.a) |*dcdz_n, dcdy_n, z_n, a_n| {
+    fn backward(self: *const PReLU, dcdz: []f64) void {
+        for (dcdz, self.d, self.z, self.a) |*dcdz_n, dcdy_n, z_n, a_n| {
             dcdz_n.* = if (z_n > 0.0) dcdy_n else dcdy_n * a_n;
         }
         return;
     }
 
     // a -= α ⋅ dc/da
-    fn learn(self: *const PReLU, dcdy: []f64, learn_rate: f64) void {
-        for (self.a, self.z, dcdy) |*a_n, z_n, dcdy_n| {
+    fn learn(self: *const PReLU, learn_rate: f64) void {
+        for (self.a, self.z, self.d) |*a_n, z_n, dcdy_n| {
             if (z_n < 0.0) a_n.* -= learn_rate * dcdy_n * z_n;
         }
         return;
     }
 };
 
-test "PReLU.forward, PReLU.backward, and PReLU.learn" {
-    const ArrF64 = Array{ .allocator = testing.allocator };
+test "Layer.init, Layer.deinit, Layer.forward, Layer.backward, and Layer.learn" {
+    const ArrF64 = Array{ .allocator = std.testing.allocator };
+    const prng: NormPrng = try NormPrng.init(std.testing.allocator, null);
+    defer prng.deinit(std.testing.allocator);
 
-    const z: []f64 = try ArrF64.vector(3);
-    inline for (.{
-        -0x1.7f2e48e8a71dfp-3,
-        -0x1.4f0d844d013aap-3,
-        -0x1.16872b020c49cp-5,
-    }, z) |v, *p| p.* = v;
+    const layer = try Layer.init(
+        std.testing.allocator,
+        &.{ .sz_i = 2, .sz_o = 3, .prng = &prng },
+    );
+    defer layer.deinit(std.testing.allocator);
 
-    const a: []f64 = try ArrF64.vector(3);
-    inline for (.{
+    inline for (layer.dense.w[0], .{
+        0x1.47ae147ae147bp-6,
+        -0x1.1eb851eb851ecp-4,
+    }) |*p, v| p.* = v;
+    inline for (layer.dense.w[1], .{
+        0x1.28f5c28f5c28fp-2,
+        -0x1.0a3d70a3d70a4p-3,
+    }) |*p, v| p.* = v;
+    inline for (layer.dense.w[2], .{
+        0x1.c28f5c28f5c29p-4,
+        -0x1.eb851eb851eb8p-6,
+    }) |*p, v| p.* = v;
+
+    inline for (layer.dense.b, .{
+        -0x1.5c28f5c28f5c3p-3,
+        -0x1.851eb851eb852p-3,
+        -0x1.999999999999ap-5,
+    }) |*p, v| p.* = v;
+
+    inline for (layer.dense.x, .{
+        0x1.d70a3d70a3d71p-3,
+        0x1.3d70a3d70a3d7p-2,
+    }) |*p, v| p.* = v;
+
+    inline for (layer.prelu.a, .{
         0x1.851eb851eb852p-3,
         0x1.28f5c28f5c28fp-2,
         0x1.5c28f5c28f5c3p-3,
-    }, a) |v, *p| p.* = v;
+    }) |*p, v| p.* = v;
 
-    const prelu: PReLU = .{ .z = z, .a = a };
-    defer prelu.deinit(ArrF64);
+    inline for (layer.prelu.d, .{ // dc/dy
+        0x1.c28f5c28f5c29p-4,
+        0x1.5c28f5c28f5c3p-3,
+        0x1.0a3d70a3d70a4p-3,
+    }) |*p, v| p.* = v;
 
-    // test "PReLU.forward"
+    // test "Layer.forward"
     const y: []f64 = try ArrF64.vector(3);
     defer ArrF64.free(y);
 
-    prelu.forward(y);
-    try testing.expect(std.mem.eql(f64, y, &.{
+    layer.forward(y);
+    try std.testing.expect(std.mem.eql(f64, y, &.{
         -0x1.2337a80cf9e39p-5,
         -0x1.84a9478c868bap-5,
         -0x1.7acc4ef88b978p-8,
     }));
 
-    // test "PReLU.backward"
-    const dcdy: []f64 = try ArrF64.vector(3);
-    inline for (.{
-        0x1.c28f5c28f5c29p-4,
-        0x1.5c28f5c28f5c3p-3,
-        0x1.0a3d70a3d70a4p-3,
-    }, dcdy) |v, *p| p.* = v;
-    defer ArrF64.free(dcdy);
+    // test "Layer.backward"
+    const dcdx: []f64 = try ArrF64.vector(2);
+    defer ArrF64.free(dcdx);
 
-    const dcdz: []f64 = try ArrF64.vector(3);
-    defer ArrF64.free(dcdz);
+    layer.backward(dcdx, 0.05);
 
-    prelu.backward(dcdz, dcdy);
-    try testing.expect(std.mem.eql(f64, dcdz, &.{
+    try std.testing.expect(std.mem.eql(f64, layer.dense.w[0], &.{
+        0x1.43bdfaa1f22dbp-6,
+        -0x1.200c019602357p-4,
+    }));
+    try std.testing.expect(std.mem.eql(f64, layer.dense.w[1], &.{
+        0x1.2861233086b08p-2,
+        -0x1.0bce131de9f5dp-3,
+    }));
+    try std.testing.expect(std.mem.eql(f64, layer.dense.w[2], &.{
+        0x1.c184dd49f2926p-4,
+        -0x1.f121e0fb57fa9p-6,
+    }));
+    try std.testing.expect(std.mem.eql(f64, layer.dense.b, &.{
+        -0x1.5e4cd74927914p-3,
+        -0x1.8a2b1704ff434p-3,
+        -0x1.a2a6f3f52fc27p-5,
+    }));
+    try std.testing.expect(std.mem.eql(f64, layer.dense.d, &.{
         0x1.566cf41f212d8p-6,
         0x1.93dd97f62b6afp-5,
         0x1.6a161e4f765fep-6,
     }));
-
-    // test "PReLU.learn"
-    prelu.learn(dcdy, 0.05);
-    try testing.expect(std.mem.eql(f64, prelu.a, &.{
+    try std.testing.expect(std.mem.eql(f64, layer.prelu.a, &.{
         0x1.873a3d12b005fp-3,
         0x1.2a624c2572805p-2,
         0x1.5c9cd3e0bd44ap-3,
+    }));
+    try std.testing.expect(std.mem.eql(f64, dcdx, &.{
+        0x1.18eb8950763a2p-6,
+        -0x1.17acc4ef88b98p-7,
     }));
 }
